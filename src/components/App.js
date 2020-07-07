1efc65cd9fa4020ec7f721b094c3a3720f4525d6
sohailkhan24 <48204572+sohailkhan24@users.noreply.github.com>
2020-07-07 21:59:50 +0530

    added data,moviecard,navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-
-
+import { data }  from '../data';
+import Navbar from './Navbar';
+import   fromimport MovieCard from './MovieCard';
+ './MovieCard'
+// map-iterate
 
 function App() {
   return (
     <div className="App">
-     Project setup
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div  className="movies">Movies</div>
+          <div  className="favourite">Favourite</div>
+        </div>
+        <div className="list">
+          
+             {data.map(movie =>(
+              <MovieCard movie ={movie}/>
+             ))} 
+        </div>
+      </div>
+    
     </div>
   );
 }
