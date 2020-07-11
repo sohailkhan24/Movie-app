8a1f0dc58234663a61da9dfd053a63700cd64b0d
sohailkhan24 <48204572+sohailkhan24@users.noreply.github.com>
2020-07-11 12:19:57 +0530

    changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,13 @@
-// import movies from "../reducers";
+import movies from "../reducers";
 // {
 //     type:'ADD_MOVIES'
     // user:[]
     //movies:[m1,m2,m3]
 // }
+
 // {
 //     type:'DECREASE_COUNT'
 // }
-//action time these variables are called
-
 export const ADD_MOVIES='ADD_MOVIES';
 //action creaters
 export function addMovies (movies){
